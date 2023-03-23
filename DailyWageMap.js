905cb4cf861f516b087f7b346827475c1eb4c2d4
Tanujachaugule <tanujachaugule1999@gmail.com>
2023-03-23 17:15:07 +0530

    Added: Daily Wage With Total Wage Using Map

---
+++ DailyWageMap.js
@@ -0,0 +1,42 @@
+const IS_Part_Time = 1;
+const IS_Full_Time = 2;
+const Part_Time_hours = 4;
+const Full_Time_Hours = 8;
+const Wage_Per_Hour =20;
+const Num_Of_Working_Days = 20;
+const Max_Hours_in_Month = 160;
+
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_Part_Time:
+            return Part_Time_hours;
+        case IS_Full_Time:
+            return Full_Time_Hours;
+            default:
+                return 0;
+    }
+}
+    function calculateDailyWage(empHours){
+        return empHours * Wage_Per_Hour;
+}
+let totalEmpHours = 0;
+let totalWorkingDays = 0;
+let empDAilyWageArr = new Array();
+let empDailyWageMap = new Map();
+
+while (totalEmpHours <= Max_Hours_in_Month && totalWorkingDays < Num_Of_Working_Days){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHours =getWorkingHours(empCheck);
+    totalEmpHours += empHours;
+    empDAilyWageArr.push(calculateDailyWage(empHours));
+    empDailyWageMap.set(totalWorkingDays, calculateDailyWage(empHours));
+}
+console.log(empDailyWageMap);
+function totalWage(totalWage,dailyWage){
+    return totalWage + dailyWage;
+}
+console.log("UC7A - Emp Wage Map totalHours: "+Array.from(empDailyWageMap.values()).reduce(totalWage,0));
+
+let empWage = calculateDailyWage(totalEmpHours);
+console.log("UC6 -Total Days: "+totalWorkingDays + "Total Hours: "+totalEmpHours +"Emp Wage: "+empWage);
